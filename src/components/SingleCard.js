165661ada83b7380daa8fe27bccd1b6fb2874d6b
Vineet Verma <vineet@styldod.com>
2021-11-05 13:16:47 +0530

    Built Memory Game

---
+++ src/components/SingleCard.js
@@ -0,0 +1,21 @@
+import './SingleCard.css'
+
+export default function SingleCard({ card, handleChoice, flipped, disabled }) {
+
+    const handleClick = () => {
+        console.log('d')
+        if (!disabled) {
+            console.log('e')
+            handleChoice(card)
+        }
+    }
+
+    return (
+        <div className="card">
+            <div className={flipped ? "flipped" : ""}>
+                <img className="front" src={card.src} alt="card front" />
+                <img className="back" src="/img/cover.png" onClick={handleClick} alt="cover" />
+            </div>
+        </div>
+    )
+}
